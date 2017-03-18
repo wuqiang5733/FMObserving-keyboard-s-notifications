37769b572253eba82cec9dac9086e260923e9b9a
xiaoqianghou <2662651103@qq.com>
2017-03-18 22:04:43 +0800

    Observing keyboard's notifications

--- Observing keyboard's notifications/ViewController.swift
+++ Observing keyboard's notifications/ViewController.swift
@@ -6,20 +6,36 @@
 //  Copyright © 2017年 WuQiang. All rights reserved.
 //
 
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightBottom: NSLayoutConstraint!
+    @IBOutlet weak var mainText: UITextView!
+    var constraintHeight: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        constraintHeight = heightBottom.constant
+        
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(openKeyboard(notification:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        center.addObserver(self, selector: #selector(closeKeyboard(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func openKeyboard(notification: Notification) {
+        if let info = notification.userInfo {
+            let value = info[UIKeyboardFrameEndUserInfoKey] as! NSValue
+            let frame = value.cgRectValue
+            let newHeight = frame.size.height + constraintHeight
+            heightBottom.constant = newHeight
+        }
     }
-
-
+    func closeKeyboard(notification: Notification) {
+        heightBottom.constant = constraintHeight
+    }
+    @IBAction func saveText(_ sender: UIButton) {
+        mainText.resignFirstResponder()
+    }
 }
-
